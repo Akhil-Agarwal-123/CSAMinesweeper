e4c133ba99fa7e213c7606c50a8fb262a7225afd
ArjunSahlot <iarjun.sahlot@gmail.com>
2024-04-25 10:38:52 -0700

    feat: generalize crazy house game mode

--- src/GameModes/CrazyHouseModeGame.java
+++ src/GameModes/CrazyHouseModeGame.java
@@ -4,6 +4,7 @@ import BoardUtil.GameStatus;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class CrazyHouseModeGame extends NormalModeGame{
     public CrazyHouseModeGame(int dim, int mines, double clusteringThreshold, int h, int w) {
@@ -24,18 +25,16 @@ public class CrazyHouseModeGame extends NormalModeGame{
 
         // generate a new board with special mine mask
         double[][] mineMask = new double[board.getDimension()][board.getDimension()];
-        int[] dij = new int[] { -1, 0, 1 };
         for (int a = 0; a < board.getDimension(); a++) {
             for (int b = 0; b < board.getDimension(); b++) {
                 if (board.getVisited(a, b)) {
                     mineMask[a][b] = 0;
                 } else {
                     mineMask[a][b] = 1;
-                    for (int di : dij) {
-                        for (int dj : dij) {
-                            if (board.inRange(a + di, b + dj) && board.getVisited(a + di, b + dj)) {
-                                mineMask[a][b] = 2.5;
-                            }
+                    ArrayList<int[]> dij = board.getNeighbors(a, b);
+                    for (int[] neighbor : dij) {
+                        if (board.getVisited(neighbor[0], neighbor[1])) {
+                            mineMask[a][b] = 2.5;
                         }
                     }
                 }
